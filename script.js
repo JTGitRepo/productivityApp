ea86c2e103b17b6f14df61f9a05c0825e0e5c5a2
John Todd <jatoddii@gmail.com>
2020-05-07 15:27:09 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,90 @@
+// Title: productivity app
+// Author: John Todd
+// Description: drap and drop tile based app allowing you to add, remove, and 
+// configure text blobs in useful ways
+
+
+
+
+// --- Main functions
+
+// returns an element of specified kind with one attribute setActive
+function createElementSetAttribute(el, attType, att, text){
+	el = document.createElement(el);
+	el.setAttribute(attType, att);
+
+	
+	return el;
+}
+
+
+// creates a text node secified by text argument and appends it to specified element
+function setText(el, text) {
+	var textNode = document.createTextNode(text);
+	el.appendChild(textNode)
+}
+
+function removeElement(el){
+	el.parentElement.remove();
+	
+}
+
+// --- Main Fuctions 
+
+
+
+
+// --- Application main body
+
+function addElement() {
+	
+	// create elements
+	var container = createElementSetAttribute("div", "class", "fieldContainer");
+		container.setAttribute("draggable", "true")
+	container.setAttribute("ondragstart", "drag(event)");
+	
+	var removeButton = createElementSetAttribute("div", "class", "removeButton");
+	removeButton.setAttribute("onclick", "removeElement(this)");
+	
+	var newEl = createElementSetAttribute("div", "class", "newField");
+	newEl.setAttribute("contenteditable", "");
+	
+	
+	
+	// add x character to removebutton to make it visible
+	setText(removeButton, "X");
+	
+	container.appendChild(removeButton);
+	container.appendChild(newEl);
+
+
+	var main = document.getElementById("main");
+	main.appendChild(container);
+}
+
+// --- Application main body
+
+
+
+addElement()
+
+
+
+
+function allowDrop(ev)
+	{
+		ev.preventDefault();
+	}
+
+function drag(ev)
+	{
+		ev.dataTransfer.setData("Text",ev.target.id);
+	}
+
+function drop(ev)
+	{
+		var data=ev.dataTransfer.getData("Text");
+		ev.target.appendChild(document.getElementById(data));
+		ev.preventDefault();
+	}
+	
